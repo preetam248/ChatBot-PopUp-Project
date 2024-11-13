ccc8e8ffcb21897b0009a4ac344dc5c735109a14
Preetam <preetamdas248@gmail.com>
2024-11-13 19:26:59 +0530

    optimize

--- script.js
+++ script.js
@@ -1,42 +1,62 @@
 document.getElementById("icon-holder").addEventListener(
   "click",
   function () {
-      let hold = document.getElementById("chat-box");
-      //   console.log(hold);
-      hold.style.display = "block";
+    let hold = document.getElementById("chat-box");
+    //   console.log(hold);
+    hold.style.display = "block";
   },
   false
 );
 document.getElementById("cross").addEventListener(
   "click",
   function () {
-      let hold = document.getElementById("chat-box");
-      //   console.log(hold);
-      hold.style.display = "none";
+    let hold = document.getElementById("chat-box");
+    //   console.log(hold);
+    hold.style.display = "none";
   },
   false
 );
 
-document.getElementById("send").addEventListener("click", function () {
-  let chatWindow = document.getElementById("chat-window");
-  let parentDiv = document.createElement("div");
-  let roundChildDiv = document.createElement("div");
-  let squareChildDiv = document.createElement("div");
+function sendMsg() {
+  let inputMsg = document.getElementById("inputText").value;
+  if (inputMsg !== "") {
   
-  let letter = document.createTextNode("P");
-  roundChildDiv.appendChild(letter);
-  roundChildDiv.classList.add("round");
+    let chatWindow = document.getElementById("chat-window");
+    let parentDiv = document.createElement("div");
+    let roundChildDiv = document.createElement("div");
+    let squareChildDiv = document.createElement("div");
+
+    let letter = document.createTextNode("P");
+    roundChildDiv.appendChild(letter);
+    roundChildDiv.classList.add("round");
+
+    let inputText = document.getElementById("inputText").value;
+    let msg = document.createTextNode(`${inputText}`);
+    squareChildDiv.appendChild(msg);
+    squareChildDiv.classList.add("square");
+
+    parentDiv.appendChild(roundChildDiv);
+    parentDiv.appendChild(squareChildDiv);
+    parentDiv.classList.add("primary");
+    parentDiv.classList.add("reverse");
+    chatWindow.appendChild(parentDiv);
+    document.getElementById("inputText").value = "";
+    chatWindow.scrollTop = chatWindow.scrollHeight; //*imp
+    }
+}
   
+document.getElementById("send").addEventListener(
+  "click",
+  sendMsg,
+  false
+);
 
-  let inputText = document.querySelector(".secondary > input").value;
-  let msg = document.createTextNode(`${inputText}`);
-  squareChildDiv.appendChild(msg);
-  squareChildDiv.classList.add("square");
-
-  parentDiv.appendChild(roundChildDiv);
-  parentDiv.appendChild(squareChildDiv);
-  parentDiv.classList.add("primary");
-  parentDiv.classList.add("reverse");
-  chatWindow.appendChild(parentDiv);
-
-},false)
+document.querySelector(".secondary > input").addEventListener(
+  "keydown",
+  function (event) {
+    if (event.key === "Enter") {
+      sendMsg();
+    }
+  },   
+  false
+);
