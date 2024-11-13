0ea9e5eff32549151f83472e1e01b35aa28717c3
Preetam <preetamdas248@gmail.com>
2024-11-13 18:14:14 +0530

    feature add

--- script.js
+++ script.js
@@ -17,3 +17,26 @@ document.getElementById("cross").addEventListener(
   false
 );
 
+document.getElementById("send").addEventListener("click", function () {
+  let chatWindow = document.getElementById("chat-window");
+  let parentDiv = document.createElement("div");
+  let roundChildDiv = document.createElement("div");
+  let squareChildDiv = document.createElement("div");
+  
+  let letter = document.createTextNode("P");
+  roundChildDiv.appendChild(letter);
+  roundChildDiv.classList.add("round");
+  
+
+  let inputText = document.querySelector(".secondary > input").value;
+  let msg = document.createTextNode(`${inputText}`);
+  squareChildDiv.appendChild(msg);
+  squareChildDiv.classList.add("square");
+
+  parentDiv.appendChild(roundChildDiv);
+  parentDiv.appendChild(squareChildDiv);
+  parentDiv.classList.add("primary");
+  parentDiv.classList.add("reverse");
+  chatWindow.appendChild(parentDiv);
+
+},false)
